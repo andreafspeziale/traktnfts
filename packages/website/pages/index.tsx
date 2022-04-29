2d56a9eb6e9c179374662927ef5a7b93b80f896b
Andrea Speziale <andreaf.speziale@gmail.com>
2022-04-29 23:08:52 +0200

    feat(website): add footer

--- packages/website/pages/index.tsx
+++ packages/website/pages/index.tsx
@@ -179,6 +179,27 @@ export function Index() {
           </Flex>
         </div>
       </div>
+      <div className="wrapper">
+        <div className="container">
+          <p id="love">
+            Carefully crafted with
+            <svg
+              fill="currentColor"
+              stroke="none"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+            by andrea.eth
+          </p>
+        </div>
+      </div>
     </StyledPage>
   );
 }

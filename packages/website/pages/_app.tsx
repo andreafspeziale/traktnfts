5241e6b8ae4a4c0fa22278fc33ad1d9f06291535
Daniele Cammareri <daniele.cammareri@colombo.it>
2022-04-29 18:09:24 +0200

    feat(website): add sanity integration

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -1,17 +1,26 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>Welcome to website!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
-    </>
+    </QueryClientProvider>
   );
 }
 
